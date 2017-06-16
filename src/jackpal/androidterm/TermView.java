f4f4e2cdfd8ac8754baf109814fe45038045f088
wangchen11 <1012371864@qq.com>
2017-06-16 18:35:27 +0800

    增加 控制台的复制粘贴功能。

--- src/jackpal/androidterm/TermView.java
+++ src/jackpal/androidterm/TermView.java
@@ -16,8 +16,20 @@
 
 package jackpal.androidterm;
 
+import person.wangchen11.editor.edittext.ViewToShowPopupMenu;
+import person.wangchen11.xqceditor.R;
 import android.content.Context;
+import android.support.v7.widget.PopupMenu;
+import android.support.v7.widget.PopupMenu.OnMenuItemClickListener;
+import android.text.ClipboardManager;
 import android.util.DisplayMetrics;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+import android.widget.Toast;
 
 import jackpal.androidterm.emulatorview.ColorScheme;
 import jackpal.androidterm.emulatorview.EmulatorView;
@@ -25,12 +37,84 @@ import jackpal.androidterm.emulatorview.TermSession;
 
 import jackpal.androidterm.util.TermSettings;
 
+@SuppressWarnings("deprecation")
 public class TermView extends EmulatorView {
 
     public TermView(Context context, TermSession session, DisplayMetrics metrics) {
         super(context, session, metrics);
     }
+    
+    @Override
+    protected void onCreateContextMenu(ContextMenu menu) {
+    	showMenu();
+    	super.onCreateContextMenu(menu);
+    }
 
+	public void showMenu()
+	{
+		PopupMenu popupMenu=new PopupMenu(this.getContext(), chooseViewToPopMenu(this) );
+		Menu menu=popupMenu.getMenu();
+		menu.add(0, R.string.selected_and_copy, 0, R.string.selected_and_copy);
+		menu.add(0, android.R.id.paste, 0, android.R.string.paste);
+		popupMenu.setOnMenuItemClickListener(new OnMenuItemClickListener() {
+			@Override
+			public boolean onMenuItemClick(MenuItem arg0) {
+				int id=arg0.getItemId();
+				switch (id) {
+				case R.string.selected_and_copy:
+					Toast.makeText(getContext(), R.string.into_copy_mode, Toast.LENGTH_SHORT).show();
+					toggleSelectingText();
+					break;
+
+				case android.R.id.paste:
+					ClipboardManager clipboardManager= (ClipboardManager) getContext().getSystemService(Context.CLIPBOARD_SERVICE);
+					CharSequence charSequence=clipboardManager.getText();
+					if(charSequence!=null)
+						TermView.this.getTermSession().write(charSequence.toString());
+					break;
+
+				default:
+					break;
+				}
+				return true;
+			}
+		});
+		popupMenu.show();
+	}
+
+	private View getViewToShowPopupMenu(View view)
+	{
+		if(view instanceof ViewToShowPopupMenu )
+		{
+			return view;
+		}
+		if(view instanceof ViewGroup)
+		{
+			ViewGroup viewGroup=(ViewGroup) view;
+			int count=viewGroup.getChildCount();
+			for(int i=0;i<count;i++)
+			{
+				View ret = getViewToShowPopupMenu(viewGroup.getChildAt(i));
+				if(ret!=null)
+					return ret;
+			}
+		}
+		return null;
+	}
+	
+	private View chooseViewToPopMenu(View view)
+	{
+		ViewParent parent= view.getParent();
+		if(parent!=null&&parent instanceof ViewGroup)
+		{
+			ViewGroup viewGroup=(ViewGroup) parent;
+			View ret = getViewToShowPopupMenu( viewGroup );
+			if(ret!=null)
+				return ret;
+		}
+		return this;
+	}
+	
     public void updatePrefs(TermSettings settings, ColorScheme scheme) {
         if (scheme == null) {
             scheme = new ColorScheme(settings.getColorScheme());
